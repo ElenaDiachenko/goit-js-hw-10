589b057e15baf5f2b8950179fcb9ee973e328aa8
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-06-21 23:08:46 +0300

    added refs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 1000;
+const searchBox = document.querySelector('#search-box')
+const countryList= document.querySelector('.country-list')
+const countryInfo = document.querySelector('.country-info')
+
+searchBox.addEventListener('input', debounce(onSerchInput, DEBOUNCE_DELAY))
 
 function fetchCountries(name) {
     fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags,languages`)
@@ -11,8 +17,3 @@ function fetchCountries(name) {
     return response.json();
   })
 }
-
-
-fetchCountries('usa')
-
-  
